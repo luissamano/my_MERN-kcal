c1ec2371fb0f93092b5c6ae78296d8c36ef9e4a5
Luis Samano <martindjt@hotmail.com>
2019-10-05 01:22:48 -0500

    add eslint

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-require('./config/config')
+require("./config/config");
 
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const { mongoose } = require('./db/mongoose')
+const { mongoose } = require("./db/mongoose");
 
+const app = express();
+const port = process.env.PORT;
 
-const app = express()
-const port = process.env.PORT
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+const routes = require("./routes");
 
-const routes = require('./routes')
+routes(app);
 
-routes(app)
 
 app.listen(port, err => {
   if (err) {
-    return console.log('Ocurrio un error', err)
+    return console.log("Ocurrio un error", err);
   }
 
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
