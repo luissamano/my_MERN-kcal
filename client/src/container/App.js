8df1639419d156c4913e49d52bb4d5e64896a20d
Luis Samano <martindjt@hotmail.com>
2019-10-09 01:05:37 -0500

    fab icon

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,28 +1,38 @@
 import React from "react";
 import Loadable from "react-loadable";
 import { Route, Switch } from "react-router-dom";
+import styles from "./styles.module.css";
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const dynamicImport = importingComponent => (
   Loadable({
     loader: importingComponent,
-    loading: () => <div style={{textAlign: 'center', marginTop: 'Sem'}}>Cargando...</div>
+    loading: () => (
+      // <div style={{ textAlign: "center", marginTop: "Sem" }}>Cargando...</div>
+      <div>
+        <LinearProgress />
+        <h5 className={styles.title}>Cargando...</h5>
+      </div>
+    )
   })
-);
+)
 
 const Calories = dynamicImport(() => import("./Pages/Calories"));
-const Dashboard = dynamicImport(() => import('./Pages/Dashboard'));
+const Dashboard = dynamicImport(() => import("./Pages/Dashboard"));
 const NoMatch = dynamicImport(() => import("./Pages/NoMatch"));
 const Summary = dynamicImport(() => import("./Pages/Summary"));
 
-const App = (props) => {
+const App = props => {
   const { history } = props;
   return (
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/add-calories" component={Calories} />
-      <Route path="/summary" component={Summary} />
-      <Route component={NoMatch} />
-    </Switch>
+    <div className={styles.container}>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/add-calories" component={Calories} />
+        <Route path="/summary" component={Summary} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
   );
 };
 
--- client/src/container/Pages/Dashboard/index.js
+++ client/src/container/Pages/Dashboard/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import styles from "./style.module.css";
 
-const Dashboard = () => 
-    <div>
-        <h1>Dashboard</h1>
-        <ul>
-            <li to="/Calories">Calories</li>
-            <li to="/Summary">Summary</li>
-        </ul>
-    </div>
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+class Dashboard extends React.Component {
+    render () {
+        return (
+            <div className={styles.container}>
+                <h1 className={styles.title}>Consumo de calorias.</h1>
+                <div className={styles.section}>
+                    <h3>Listado de calorias consumidas.</h3>
+                    <div className={styles.containerBtns}>
+                        <Fab
+                            size='medium'
+                            colors='primary'
+                            arial-label='Add'
+                        >
+                            <AddIcon />
+                        </Fab>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Dashboard;
